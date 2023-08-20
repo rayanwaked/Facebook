03f076e6e3a32b54431bfa5ab365b3dd4adb7755
Rayan Waked <rayanwaked@icloud.com>
2023-08-20 12:20:36 -0700

    Tabs are now functional     - Needs the rest of the Home screen to be integrated

---
+++ Facebook/Home/Tabs/TabsModel.swift
@@ -0,0 +1,18 @@
+//
+//  TabsModel.swift
+//  Facebook
+//
+//  Created by Rayan Waked on 8/20/23.
+//
+
+import SwiftUI
+
+struct TabsModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabsModel()
+}
---
+++ Facebook/Home/Tabs/TabsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  TabsViewModel.swift
+//  Facebook
+//
+//  Created by Rayan Waked on 8/20/23.
+//
+
+import SwiftUI
+
+struct TabsViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabsViewModel()
+}
